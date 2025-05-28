1a7e44bb5bc4e6c143032750d7c97e1c00cc3bc0
lastbytee <66225889+lastbytee@users.noreply.github.com>
2025-05-28 11:06:00 +0000

    new

--- surveymaker/settings.py
+++ surveymaker/settings.py
@@ -62,13 +62,14 @@ WSGI_APPLICATION = 'surveymaker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'), 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
