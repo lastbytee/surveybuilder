56f3902cc143c4c22372c2511156dfd7f41b7828
lastbytee <66225889+lastbytee@users.noreply.github.com>
2025-05-30 06:53:00 +0000

    a complete survey maker project

--- surveymaker/settings.py
+++ surveymaker/settings.py
@@ -6,13 +6,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = 'django-insecure-8#5x)_dhcu=lnn-z$_kd%pkd*!4ff@pt5vfwl7q^%03j=k09@z'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # APPLICATIONS
 INSTALLED_APPS = [
